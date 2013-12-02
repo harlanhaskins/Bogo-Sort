8c813ee65165aff256a58de9d7a84883e9bbe2ea
Harlan Haskins <harlan@harlanhaskins.com>
2013-12-02 12:44:46 -0500

    Added title bar to Python implementation.

---
+++ Python/main.py
@@ -0,0 +1,15 @@
+from BogoSort import *
+from BogoSortAnalysis import *
+
+def main():
+    print("Welcome to BogoSort. Please enter the number of what you'd like to do.\n");
+    print("\t1. Run a BogoSort");
+    print("\t2. Save a series of BogoSorts");
+    print("\t3. Quit");
+    menuChoice = int(input("\nWhat would you like to do? "))
+
+    choices = [exit, runBogoSort, runAnalysis, exit];
+
+    choices[menuChoice]()
+
+main()
